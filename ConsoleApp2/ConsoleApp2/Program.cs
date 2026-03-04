b6457cdbef9cae937172b7d99996e1784a2fa829
ilkin200500 <ikoyamda1@gmail.com>
2026-03-04 19:01:52 +0400

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,227 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int cem = 0;
+            //for(int j = 1; j < 50; j++)
+            //{
+            //   cem = cem + j;
+
+            //}
+            //Console.WriteLine(cem);
+            //1 den 20 eqeder olan cut ededlerin cemini tapin
+            //int cem = 0;
+            //for (int m = 0; m < 20; m++)
+            //{
+            //    if (m % 2 == 0)
+            //    {
+            //        cem = cem + m;
+            //    }
+            //}
+            //Console.WriteLine(cem);
+            //1 den 20 e qeder tek ededleri konsola cap edin
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //1 den 100 e qeder ededlerin ortalamasini tapin
+            //int eo = 0;
+            //int cem = 0;
+            //int say = 0;
+            //for(int i = 1; i < 100; i++)
+            //{
+            //    cem = cem + i;
+            //    say += 1;
+
+            //}
+            //Console.WriteLine(cem / say);
+            //1 den 100 e qeder olan ededlerden 5 ve 7 e bolunenleri tap ve cap et
+
+            //1 den 10 a qeder ededleri dovr ederek cut ve tek ededlerin cemini ayri ayriliqda tap
+            //int cemtek = 0;
+            //int cemcut = 0;
+            //for(int i = 1; i < 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        cemcut = cemcut + i;
+            //    }
+            //    else
+            //    {
+            //        cemtek = cemtek + i;
+            //    }
+            //}
+            //Console.WriteLine($"tekededlerin cemi={cemtek},cut ededlerin cemi={cemcut}");
+
+            //int bal = 85;
+            //if (bal > 90 && bal<100)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (bal>80 && bal < 90)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if(bal>70 && bal < 80)
+            //{
+            //    Console.WriteLine("C")
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ugursuz");
+            //}
+            //arrayin butun elementlerinin cemini qaytaran funksiya yazin
+            //int cem = 0;
+            //int[] Arr=[12, 14, 35, 56, 68, 93];
+            //for(int i = 0; i < Arr.Length; i++)
+            //{
+            //    cem = cem + i;
+            //}
+            //Console.WriteLine(cem);
+            //arrayin tek index elementlerini tapin
+
+            //int[] Arr = [12, 14, 35, 56, 68, 93];
+            //for (int i = 0;i<Arr.Length;i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(Arr[i]);
+            //    }
+            //}
+            //arrayin cut indeksinde duran elementler
+            //int[] Arr = [12, 14, 35, 56, 68, 93];
+            //for (int i = 0;i<Arr.Length;i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(Arr[i]);
+            //    }
+            //}
+            // 10 dan 1 e qeder ededleri ter sirada cap et
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            // daxil edilen yasa gore istifadecinin pensiyay yasina catib  catmadigini tapan proqram
+            //Console.WriteLine("yasi daxil edin");
+            //int yas = int.Parse(Console.ReadLine());
+            //if (yas > 65 &&yas<125)
+            //{
+            //    Console.WriteLine("pensiya dusur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("pensiya dusmur");
+            //}
+
+            //daxil edilen reqemin cut yoxsa tek olmasini tapin eded musbet olmalidir
+            //Console.WriteLine("reqemi daxil edin");
+            //int reqem = int.Parse(Console.ReadLine());
+            //if (reqem > 0)
+            //{
+            //    if (reqem % 2 == 0)
+            //    {
+            //        Console.WriteLine("eded cutdur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("eded tekdir");
+            //    }
+            //}
+            // daxil edilen musbet ededin 3 ve 5 ededine eyni anda bolunub bolunmediyini tapin
+            //int eded = int.Parse(Console.ReadLine());
+            //if (eded > 0)
+            //{
+            //    if(eded%3==0 && eded % 5 == 0)
+            //    {
+            //        Console.WriteLine("eded hem 3 e hem 5 e bolunur");
+            //    }
+            //}
+            // daxil edile musbet ededin 2 e tam bolunub bolunmediyini tapan proqram yazin eger tam bolunurse consolda bolundu eger tam bolunmurse qaliq tapilsin
+            //Console.WriteLine("eded daxil edin");
+            //int eded = int.Parse(Console.ReadLine());
+            //if (eded > 0)
+            //{
+            //    if (eded % 2 == 0)
+            //    {
+            //        Console.WriteLine("eded  2 e bolunur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(eded% 2);
+            //    }
+            //}
+            // daxil edilen 2 ededin hasilinin musbet yada menfi olmasini gosteren prqoram
+            //Console.WriteLine("eded1 i daxil edin");
+            //    int eded1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("eded2 i daxil edin");
+            //int eded2 = int.Parse(Console.ReadLine());
+            //int hasil = eded1 * eded2;
+            //if (hasil > 0)
+            //{
+            //    Console.WriteLine("eded musbetdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded menfidir");
+            //}
+            //userden name ve username qebul edin ve fullnameini ekranda cap edin
+            //Console.WriteLine("name i daxil edin");
+            //String name = Console.ReadLine();
+            //Console.WriteLine("surname i daxil edin");
+            //String surname = Console.ReadLine();
+            //string fullname = name + surname;
+            //Console.WriteLine(fullname);
+            //Console.WriteLine("boyu daxil edin");
+            //int boy = int.Parse(Console.ReadLine());
+            //Console.WriteLine("cekini daxil edin");
+            //int ceki = int.Parse(Console.ReadLine());
+            //int bmi = boy / ceki * ceki;
+            //if (bmi > 25)
+            //{
+            //    Console.WriteLine("artiqcekili");
+
+            //}
+            //else if(bmi>18.5 && bmi<24.9) 
+            //    {
+            //    Console.WriteLine("normal");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ariq");
+            //}
+
+            //promt vasitesile daxil olunmus ededin
+            // promt vasitesile daxil olunmush ededin faktorialini tapin numune 3 olarsa 1x 2 x3
+            //Console.WriteLine("eded daxil edin");
+            //int eded = int.Parse(Console.ReadLine());
+            //int i = 1;
+            //int f = 1;
+            //while (i < eded+1)
+            //{
+            //    f = f * i;
+            //    i = i + 1;
+
+            //}
+            //Console.WriteLine(f);
+            // verilmis arrayda tek elementlerde en boyuyunu tapin
+            
+            
+
+
+
+
+        }
+    }
+}
